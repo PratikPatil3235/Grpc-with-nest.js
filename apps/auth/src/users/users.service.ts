ffbeff58ce012e732ae93919fc55ee585ea7ff76
PratikPatil3235 <pratikpatil@medianv.com>
2025-06-30 15:02:18 +0530

    first commit

---
+++ apps/auth/src/users/users.service.ts
@@ -0,0 +1,88 @@
+import { Injectable, NotFoundException, OnModuleInit } from '@nestjs/common';
+import {
+  CreateUserDto,
+  PaginationDto,
+  UpdateUserDto,
+  User,
+  Users,
+} from '@app/common';
+import { fail } from 'assert';
+import { randomUUID } from 'crypto';
+import { Observable, Subject } from 'rxjs';
+
+@Injectable()
+export class UsersService implements OnModuleInit {
+  private readonly users: User[] = [];
+
+  onModuleInit() {
+    for (let i = 0; i < 100; i++) {
+      this.create({ username: randomUUID(), password: randomUUID(), age: 0 });
+    }
+  }
+
+  create(createUserDto: CreateUserDto): User {
+    const user: User = {
+      ...createUserDto,
+      subscribed: false,
+      socialMedia: {
+        fbUri: '',
+        twitterUri: '',
+      },
+      id: randomUUID(),
+    };
+    this.users.push(user);
+    return user;
+  }
+
+  findAll(): Users {
+    return { user: this.users };
+  }
+
+  findOne(id: string): User {
+    const user = this.users.find((user) => user.id == id);
+    if (!user) {
+      throw new NotFoundException();
+    }
+    return user;
+  }
+
+  update(id: string, updateUserDto: UpdateUserDto): User {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if (userIndex != -1) {
+      this.users[userIndex] = {
+        ...this.users[userIndex],
+        ...updateUserDto,
+      };
+      return this.users[userIndex];
+    }
+    throw new NotFoundException(`user with id ${id} not found`);
+  }
+
+  removeUser(id: string): User {
+    const userIndex = this.users.findIndex((user) => user.id === id);
+    if (userIndex != -1) {
+      return this.users.splice(userIndex)[0];
+    }
+    throw new NotFoundException(`user with id ${id} not found`);
+  }
+
+  queryUsers(
+    paginationDtoStream: Observable<PaginationDto>,
+  ): Observable<Users> {
+    const subject = new Subject<Users>();
+
+    const onNext = (paginationDto: PaginationDto) => {
+      const start = paginationDto.page * paginationDto.skip;
+      subject.next({
+        user: this.users.slice(start, start + paginationDto.skip),
+      });
+    };
+    const onComplete = () => subject.complete();
+    paginationDtoStream.subscribe({
+      next: onNext,
+      complete: onComplete,
+    });
+
+    return subject.asObservable();
+  }
+}
